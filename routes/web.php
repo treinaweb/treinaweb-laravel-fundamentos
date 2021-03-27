a8c176685f01a4e764e7de7ffacb533b8d68ed80
Elton Fonseca <elton869@gmail.com>
2021-03-27 09:14:22 -0300

    15 - Criando o primeiro Model

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Saudacao;
 use App\Http\Controllers\SiteController;
+use App\Models\Client;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,3 +27,7 @@ Route::get('/servicos', [SiteController::class, 'servicos']);
 Route::get('/servico/{id}', [SiteController::class, 'servico']);
 
 Route::get('/saudacao/{nome?}', Saudacao::class);
+
+Route::get('/clients', function() {
+    dd(Client::get());
+});

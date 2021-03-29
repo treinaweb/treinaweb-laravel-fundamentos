675535f72d6ba676e6b87a364d89760d49f6a80b
Elton Fonseca <elton869@gmail.com>
2021-03-29 08:21:18 -0300

    27 - Resource Controller

--- routes/web.php
+++ routes/web.php
@@ -29,12 +29,12 @@ Route::get('/servico/{id}', [SiteController::class, 'servico']);
 
 Route::get('/saudacao/{nome?}', Saudacao::class);
 
-Route::get('/clients', [ClientController::class, 'index'])->name('clients.index');
-Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
-Route::get('/clients/{id}', [ClientController::class, 'show'])->name('clients.show');
-Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
+// Route::get('/clients', [ClientController::class, 'index'])->name('clients.index');
+// Route::get('/clients/create', [ClientController::class, 'create'])->name('clients.create');
+// Route::get('/clients/{id}', [ClientController::class, 'show'])->name('clients.show');
+// Route::post('/clients', [ClientController::class, 'store'])->name('clients.store');
+// Route::get('/clients/{id}/edit', [ClientController::class, 'edit'])->name('clients.edit');
+// Route::put('/clients/{id}', [ClientController::class, 'update'])->name('clients.update');
+// Route::delete('/clients/{id}', [ClientController::class, 'destroy'])->name('clients.destroy');
 
-Route::get('/clients/{id}/edit', [ClientController::class, 'edit'])->name('clients.edit');
-Route::put('/clients/{id}', [ClientController::class, 'update'])->name('clients.update');
-
-Route::delete('/clients/{id}', [ClientController::class, 'destroy'])->name('clients.destroy');
+Route::resource('clients', ClientController::class);
